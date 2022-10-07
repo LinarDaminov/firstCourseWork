f23d11410daea0b7d26b184e76b684fee24ec353
LinarDaminov <linar.daminov@yandex.ru>
2022-10-07 17:57:00 +0500

    second commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,21 @@
+import java.util.Random;
+
 public class Employee {
 
     private String name;
     private int department;
     private int monthSalary;
-    private int id;
+    private static int id;
 
-    Employee Ivan = new Employee("Иван Иванов", 5, 205700);
-    Employee Petr = new Employee("Петр Петров", 3,140890);
-    Employee Dima = new Employee("Дмитрий Лебедев", 1, 230774);
-    Employee Azat = new Employee("Азат Садыков", 2, 214287);
+    // counter
+    private static int count = 0;
 
+    public Employee() {
+        this.id = count == 0 ? new Random().nextInt(1000) : count + 1;
+        count = this.id;
+    }
 
+    //getters
     public String getName() {
         return name;
     }
@@ -23,6 +28,11 @@ public class Employee {
         return monthSalary;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    //setters
     public void setDepartment(int department) {
         this.department = department;
     }
@@ -31,15 +41,26 @@ public class Employee {
         this.monthSalary = monthSalary;
     }
 
-    public Employee(String name, int department, int monthSalary) {
+    //constructor
+    public Employee(String name, int department, int monthSalary, int id) {
         this.name = name;
         this.department = department;
         this.monthSalary = monthSalary;
     }
 
+    // toString
     @Override
-    public  String toString () {
-        return  "Сотрудник:" + name + "," + " Отдел №: " + department + ","+ " Заработная плата: " +  monthSalary + " рублей";
+    public String toString() {
+        return "Сотрудник:" + name + "," + " Отдел №: " + department + "," + " Заработная плата: " + monthSalary + " рублей";
+    }
+    public int totalSalary(Employee[]employees) {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum = sum + getMonthSalary();
+            System.out.println(sum);
+        }
+        return sum;
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.println();
-        System.out.println(Petr);
-        System.out.println(Dima);
-        System.out.println(Azat);
+public class Main {
+
+    public static void main(String[] args) {
+  Employee [] employees = new Employee[10];
+        employees [0] = new Employee("Иван Иванов", 5, 205700,1);
+        employees [1] = new Employee("Петр Петров", 3,140890, 1 );
+        employees [2] = new Employee("Сергей Сергеев", 1, 230774, 1);
+        employees [3] = new Employee("Дмитрий Дмитриев", 2, 214287, 1);
+        employees [4] = new Employee("Олег Олегов", 4,149254, 1);
+        System.out.println(employees[0]);
+        System.out.println(employees[1]);
+        System.out.println(employees[2]);
+        System.out.println(employees[3]);
+        System.out.println(employees[4]);
+        System.out.println(employees[0].totalSalary());
 
 
 
