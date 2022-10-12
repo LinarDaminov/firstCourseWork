fe180dd15cd109b862338d0da713ab246a815a5b
LinarDaminov <linar.daminov@yandex.ru>
2022-10-12 21:52:37 +0500

    firstCourseWorkWithCorrect2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     private static final Employee[] EMPLOYEES = new Employee[10];
 
-    public static <EMPLOYEES> void main(String[] args) {
+    public static void main(String[] args) {
 
         EMPLOYEES[0] = new Employee("Иванов Иван", 1, 100500);
         EMPLOYEES[1] = new Employee("Петров Петр", 2, 122700);
@@ -15,18 +15,8 @@ public class Main {
         EMPLOYEES[7] = new Employee("Сергеев Сергей", 2, 127055);
         EMPLOYEES[8] = new Employee("Алексеев Алексей", 1, 119014);
         EMPLOYEES[9] = new Employee("Степанов Степан", 0, 129077);
-        //a
-        System.out.println(EMPLOYEES[0]);
-        System.out.println(EMPLOYEES[1]);
-        System.out.println(EMPLOYEES[2]);
-        System.out.println(EMPLOYEES[3]);
-        System.out.println(EMPLOYEES[4]);
-        System.out.println(EMPLOYEES[5]);
-        System.out.println(EMPLOYEES[6]);
-        System.out.println(EMPLOYEES[7]);
-        System.out.println(EMPLOYEES[8]);
-        System.out.println(EMPLOYEES[9]);
-
+        //a Общий список сотрудников со всеми данными
+        printAllNameAndAnotherInfo();
         //b
         System.out.println("Общая сумма заработной платы составляет " + totalSum() + " рублей");
         //c
@@ -35,19 +25,8 @@ public class Main {
         System.out.println("Сотрудник с минимальной заработной платой " + getMinSalary());
         //e
         System.out.println(" Средняя заработная плата составляет: " + getAverageSalary());
-        //f
+        //f Список студников без остальных даных
         printAllName();
-        //g
-        System.out.println(EMPLOYEES[0].toString());
-        System.out.println(EMPLOYEES[1].toString());
-        System.out.println(EMPLOYEES[2].toString());
-        System.out.println(EMPLOYEES[3].toString());
-        System.out.println(EMPLOYEES[4].toString());
-        System.out.println(EMPLOYEES[5].toString());
-        System.out.println(EMPLOYEES[6].toString());
-        System.out.println(EMPLOYEES[7].toString());
-        System.out.println(EMPLOYEES[8].toString());
-        System.out.println(EMPLOYEES[9].toString());
 
     }
 
@@ -85,28 +64,34 @@ public class Main {
     private static int getAverageSalary() {
         double average = 0;
         double sum = 0;
-        int numberStaff = 10;
+        int numberStaff = 0;
         for (Employee employee : EMPLOYEES) {
             if (employee != null) {
                 sum = +sum + employee.getMonthSalary();
             }
-
         }
-        average = sum / numberStaff;
-        return (int) average;
+        for (Employee employee:EMPLOYEES)
+        {
+            if(employee!=null)
+        numberStaff++;}
 
+        average=sum/numberStaff;
+    return (int) average;
     }
-    private static void printAllName(){
-        for(Employee employee:EMPLOYEES){
-            if (employee !=null){
-                System.out.println(employee.getName());
+    private static void printAllName() {
+        for (Employee employee : EMPLOYEES) {
+            if (employee != null) {
+                System.out.println(employee.getName() + ",");
             }
         }
-
-
     }
-
-
+    public static void printAllNameAndAnotherInfo(){
+        for (Employee employee : EMPLOYEES) {
+            if (employee!= null){
+                System.out.println(employee);
+            }
+        }
+    }
 }
 
 
